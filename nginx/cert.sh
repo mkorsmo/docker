1086bd7eb25d6c397eec8606a870dd0457d49187
Matt Korsmo <matt@jamplay.com>
2016-09-22 16:07:01 -0500

    Move mkdir to Docker file from cert.sh

--- nginx/cert.sh
+++ nginx/cert.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-mkdir /etc/nginx/ssl
+
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/nginx.key -out /etc/nginx/ssl/nginx.crt -subj "/C=US/ST=Minnesota/L=Minneapolis/O=IT/CN=www.jamplay.com"
